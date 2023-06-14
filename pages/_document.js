c6f4197f2f71951c4cd5cff749ef47113a628c07
Emin Atalay <atalay.gov@gmail.com>
2023-06-15 02:33:01 +0300

    Update

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default class Document extends NextDocument {
         return (
             <Html lang="en">
                 <Head />
-                <link rel="icon" href="/js.ico"/>
+                <link rel="shortcut icon" href="/favicon.ico"/>
                 <body>
                     <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
                     <Main />
