ddc565e109b443edf304a75b13b138cc4a9f3bcc
Emin Atalay <atalay.gov@gmail.com>
2023-06-15 02:45:59 +0300

    update

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import NextDocument, {Html, Head, Main, NextScript} from 'next/document'
 import theme from '../libs/theme'
-import favicon from './favicon.ico'
+import favicon from '../static/favicon.ico';
 
 
 export default class Document extends NextDocument {
