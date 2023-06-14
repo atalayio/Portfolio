4c4cba6847695d1189ae846ed4496cbd05be27aa
Emin Atalay <atalay.gov@gmail.com>
2023-06-15 02:36:47 +0300

    update

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import NextDocument, {Html, Head, Main, NextScript} from 'next/document'
 import theme from '../libs/theme'
+import favicon from './favicon.ico'
+
 
 export default class Document extends NextDocument {
     render() {
         return (
             <Html lang="en">
                 <Head />
-                <link rel="shortcut icon" href="/favicon.ico"/>
+                <link rel="shortcut icon" href={favicon}/>
                 <body>
                     <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
                     <Main />
